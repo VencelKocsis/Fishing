2d9b1200ab86e50b1c6304343cb7f7359d45ac90
VencelKocsis <katzarmondi@gmail.com>
2025-03-02 00:31:37 +0100

    Initial commit

---
+++ app/src/main/java/hu/bme/aut/android/fishing/MainActivity.kt
@@ -0,0 +1,19 @@
+package hu.bme.aut.android.fishing
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import hu.bme.aut.android.fishing.ui.theme.FishingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FishingTheme {
+
+            }
+        }
+    }
+}
