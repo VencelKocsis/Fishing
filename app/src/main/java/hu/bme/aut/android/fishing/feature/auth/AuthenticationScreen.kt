5b61455029ee2e716362614d27660f6a9f8701e7
VencelKocsis <katzarmondi@gmail.com>
2025-03-02 01:33:39 +0100

    AuthenticationScreen, AuthenticationViewModel added

---
+++ app/src/main/java/hu/bme/aut/android/fishing/feature/auth/AuthenticationScreen.kt
@@ -0,0 +1,214 @@
+package hu.bme.aut.android.fishing.feature.auth
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Key
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import hu.bme.aut.android.fishing.R
+import hu.bme.aut.android.fishing.ui.common.EmailTextField
+import hu.bme.aut.android.fishing.ui.common.PasswordTextField
+import hu.bme.aut.android.fishing.util.UiEvent
+import kotlinx.coroutines.launch
+
+@Composable
+fun AuthenticationScreen(
+    viewModel: AuthenticationViewModel = hiltViewModel()
+) {
+
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    val snackbarHostState = SnackbarHostState()
+
+    val scope = rememberCoroutineScope()
+
+    val context = LocalContext.current
+
+    LaunchedEffect(key1 = true) {
+        viewModel.checkSignInState()
+        viewModel.uiEvent.collect { event ->
+            when (event) {
+                is UiEvent.Success -> {
+                    scope.launch {
+                        snackbarHostState.showSnackbar(
+                            message = event.message?.asString(context) ?: ""
+                        )
+                    }
+                }
+
+                is UiEvent.Failure -> {
+                    scope.launch {
+                        snackbarHostState.showSnackbar(
+                            message = event.message.asString(context)
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        modifier = Modifier
+            .fillMaxSize()
+            .safeDrawingPadding()
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .padding(padding)
+                .background(MaterialTheme.colorScheme.primaryContainer)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            if (state.isLoggedIn) {
+                Button(
+                    shape = RoundedCornerShape(8.dp),
+                    onClick = { viewModel.onEvent(AuthenticationEvent.LogoutButtonClicked) },
+                    modifier = Modifier
+                        .padding(bottom = 8.dp)
+                        .width(TextFieldDefaults.MinWidth)
+                        .height(64.dp)
+                ) {
+                    Text(text = stringResource(R.string.button_text_sign_out))
+                }
+            } else {
+                EmailTextField(
+                    value = state.email,
+                    label = stringResource(id = R.string.textfield_label_email),
+                    onValueChange = { viewModel.onEvent(AuthenticationEvent.EmailChanged(it)) },
+                    onDone = {},
+                    imeAction = ImeAction.Next,
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Person,
+                            contentDescription = null
+                        )
+                    },
+                    modifier = Modifier
+                        .padding(bottom = 8.dp)
+                        .width(TextFieldDefaults.MinWidth)
+                        .height(64.dp)
+                )
+                PasswordTextField(
+                    value = state.password,
+                    label = stringResource(id = R.string.textfield_label_password),
+                    onValueChange = { viewModel.onEvent(AuthenticationEvent.PasswordChanged(it)) },
+                    onDone = {},
+                    modifier = Modifier
+                        .padding(bottom = 8.dp)
+                        .width(TextFieldDefaults.MinWidth)
+                        .height(64.dp),
+                    isVisible = state.passwordVisibility,
+                    onVisibilityChanged = { viewModel.onEvent(AuthenticationEvent.PasswordVisibilityChanged) },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Key,
+                            contentDescription = null
+                        )
+                    }
+                )
+                AnimatedVisibility(visible = state.isInRegisterMode) {
+                    PasswordTextField(
+                        value = state.confirmPassword,
+                        label = stringResource(id = R.string.textfield_label_confirm_password),
+                        onValueChange = {
+                            viewModel.onEvent(
+                                AuthenticationEvent.ConfirmPasswordChanged(
+                                    it
+                                )
+                            )
+                        },
+                        onDone = {},
+                        modifier = Modifier
+                            .padding(bottom = 8.dp)
+                            .width(TextFieldDefaults.MinWidth)
+                            .height(64.dp),
+                        isVisible = state.confirmPasswordVisibility,
+                        onVisibilityChanged = { viewModel.onEvent(AuthenticationEvent.ConfirmPasswordVisibilityChanged) },
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Default.Key,
+                                contentDescription = null
+                            )
+                        }
+                    )
+                }
+                AnimatedVisibility(visible = !state.isInRegisterMode) {
+
+                    Spacer(
+                        modifier = Modifier
+                            .padding(bottom = 8.dp)
+                            .height(64.dp)
+                    )
+                }
+                Spacer(
+                    modifier = Modifier
+                        .height(24.dp)
+                )
+                Button(
+                    shape = RoundedCornerShape(8.dp),
+                    onClick = { viewModel.onEvent(AuthenticationEvent.AuthenticationButtonClicked) },
+                    modifier = Modifier
+                        .padding(bottom = 8.dp)
+                        .width(TextFieldDefaults.MinWidth)
+                        .height(64.dp)
+                ) {
+                    Text(
+                        text =
+                        if (state.isInRegisterMode)
+                            stringResource(id = R.string.button_text_sign_up)
+                        else
+                            stringResource(id = R.string.button_text_sign_in)
+                    )
+                }
+                OutlinedButton(
+                    shape = RoundedCornerShape(8.dp),
+                    onClick = { viewModel.onEvent(AuthenticationEvent.ChangeMode) },
+                    modifier = Modifier
+                        .padding(bottom = 8.dp)
+                        .width(TextFieldDefaults.MinWidth)
+                        .height(64.dp)
+                ) {
+                    Text(
+                        text =
+                        if (state.isInRegisterMode)
+                            stringResource(id = R.string.button_text_back_to_login)
+                        else
+                            stringResource(id = R.string.button_text_no_account)
+                    )
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/hu/bme/aut/android/fishing/feature/auth/AuthenticationViewModel.kt
@@ -0,0 +1,165 @@
+package hu.bme.aut.android.fishing.feature.auth
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import hu.bme.aut.android.fishing.R
+import hu.bme.aut.android.fishing.domain.usecases.auth.AllAuthenticationUseCases
+import hu.bme.aut.android.fishing.ui.model.UiText
+import hu.bme.aut.android.fishing.ui.model.toUiText
+import hu.bme.aut.android.fishing.util.UiEvent
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AuthenticationViewModel @Inject constructor(
+    private val authentication: AllAuthenticationUseCases,
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(AuthenticationState())
+    val state = _state.asStateFlow()
+
+    private val email get() = state.value.email
+
+    private val password get() = state.value.password
+
+    private val confirmPassword get() = state.value.confirmPassword
+
+    private val _uiEvent = Channel<UiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    fun onEvent(event: AuthenticationEvent) {
+        when (event) {
+            is AuthenticationEvent.EmailChanged -> {
+                val newEmail = event.email.trim()
+                _state.update { it.copy(email = newEmail) }
+            }
+
+            is AuthenticationEvent.PasswordChanged -> {
+                val newPassword = event.password.trim()
+                _state.update { it.copy(password = newPassword) }
+            }
+
+            is AuthenticationEvent.ConfirmPasswordChanged -> {
+                val newConfirmPassword = event.password.trim()
+                _state.update { it.copy(confirmPassword = newConfirmPassword) }
+            }
+
+            AuthenticationEvent.PasswordVisibilityChanged -> {
+                _state.update { it.copy(passwordVisibility = !state.value.passwordVisibility) }
+            }
+
+            AuthenticationEvent.ConfirmPasswordVisibilityChanged -> {
+                _state.update { it.copy(confirmPasswordVisibility = !state.value.confirmPasswordVisibility) }
+            }
+
+            AuthenticationEvent.AuthenticationButtonClicked -> {
+                if (_state.value.isInRegisterMode)
+                    onSignUp()
+                else
+                    onSignIn()
+            }
+
+            AuthenticationEvent.LogoutButtonClicked -> {
+                if (_state.value.isLoggedIn)
+                    onLogOut()
+            }
+
+            AuthenticationEvent.ChangeMode -> {
+                _state.update { it.copy(isInRegisterMode = !_state.value.isInRegisterMode) }
+            }
+        }
+    }
+
+    private fun onSignIn() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                if (!authentication.isEmailValid(email)) {
+                    _uiEvent.send(
+                        UiEvent.Failure(UiText.StringResource(R.string.some_error_message))
+                    )
+                } else {
+                    if (password.isBlank()) {
+                        _uiEvent.send(
+                            UiEvent.Failure(UiText.StringResource(R.string.some_error_message))
+                        )
+                    } else {
+                        authentication.signIn(email, password)
+                        _uiEvent.send(UiEvent.Success(UiText.StringResource(R.string.successful_login)))
+                        checkSignInState()
+                    }
+                }
+            } catch (e: Exception) {
+                _uiEvent.send(UiEvent.Failure(e.toUiText()))
+            }
+        }
+    }
+
+    private fun onSignUp() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                if (!authentication.isEmailValid(email)) {
+                    _uiEvent.send(
+                        UiEvent.Failure(UiText.StringResource(R.string.some_error_message))
+                    )
+                } else {
+                    if (!authentication.passwordsMatch(password, confirmPassword) && password.isNotBlank()) {
+                        _uiEvent.send(
+                            UiEvent.Failure(UiText.StringResource(R.string.some_error_message))
+                        )
+                    } else {
+                        authentication.signUp(email, password)
+                        _uiEvent.send(UiEvent.Success(UiText.StringResource(R.string.successful_registration)))
+                        onSignIn()
+                    }
+                }
+            } catch (e: Exception) {
+                _uiEvent.send(UiEvent.Failure(e.toUiText()))
+            }
+        }
+    }
+
+    private fun onLogOut() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                authentication.signOut()
+                _uiEvent.send(UiEvent.Success(UiText.StringResource(R.string.successful_signout)))
+                checkSignInState()
+
+            } catch (e: Exception) {
+                _uiEvent.send(UiEvent.Failure(e.toUiText()))
+            }
+        }
+    }
+
+    fun checkSignInState() = _state.update { it.copy(isLoggedIn = authentication.hasUser()) }
+
+}
+
+
+data class AuthenticationState(
+    val email: String = "",
+    val password: String = "",
+    val confirmPassword: String = "",
+    val passwordVisibility: Boolean = false,
+    val confirmPasswordVisibility: Boolean = false,
+    val isInRegisterMode: Boolean = false,
+    val isLoggedIn: Boolean = false
+)
+
+sealed class AuthenticationEvent {
+    data class EmailChanged(val email: String) : AuthenticationEvent()
+    data class PasswordChanged(val password: String) : AuthenticationEvent()
+    data class ConfirmPasswordChanged(val password: String) : AuthenticationEvent()
+    object PasswordVisibilityChanged : AuthenticationEvent()
+    object ConfirmPasswordVisibilityChanged : AuthenticationEvent()
+    object AuthenticationButtonClicked : AuthenticationEvent()
+    object LogoutButtonClicked : AuthenticationEvent()
+    object ChangeMode : AuthenticationEvent()
+}
