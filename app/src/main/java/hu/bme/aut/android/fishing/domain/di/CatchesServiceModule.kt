5532a142a9acd98c3919d4f48ac8bf45db0856b6
VencelKocsis <katzarmondi@gmail.com>
2025-03-03 21:41:32 +0100

    added CatchesServiceModule

---
+++ app/src/main/java/hu/bme/aut/android/fishing/domain/di/CatchesServiceModule.kt
@@ -0,0 +1,20 @@
+package hu.bme.aut.android.fishing.domain.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import hu.bme.aut.android.fishing.data.catches.CatchService
+import hu.bme.aut.android.fishing.data.catches.FirebaseCatchService
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class CatchesServiceModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindFirebaseCatchesService(
+        firebaseCatchService: FirebaseCatchService
+    ): CatchService
+}
